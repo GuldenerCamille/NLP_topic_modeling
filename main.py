385d57bdd53c7ca77645770976c8389c20f25fa6
camille guldener <guldener.camille@gmail.com>
2023-06-02 13:46:26 +0200

    cleaning

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import gensim
-from gensim import corpora, models
-import re
-import os
-import shutil
-import heapq
 from utilitaires import send_data_to_api
 
 # Page Accueil
